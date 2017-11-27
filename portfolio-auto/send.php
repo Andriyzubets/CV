00937bc4d49373be0c27fff87b6f976b0e0494d5
Zubetsandriy <zubets.andriy@gmail.com>
2017-11-27 14:41:57 +0200

    changed cn-auto portfolio

--- portfolio-auto/send.php
+++ portfolio-auto/send.php
@@ -1,36 +1,16 @@
 <?php
 $fio = $_POST['fio'];
-$target = $_POST['target']
-$date = $_POST['date']
-$number_auto = $_POST['number_auto']
-$extra = $_POST['extra']
-$phone = $_POST['phone'];
+$email = $_POST['email'];
 $fio = htmlspecialchars($fio);
-$target = htmlspecialchars($target)
-$phone = htmlspecialchars($phone);
-$date = htmlspecialchars($date);
-$number_auto = htmlspecialchars($number_auto);
-$extra = htmlspecialchars($extra);
+$email = htmlspecialchars($email);
 $fio = urldecode($fio);
-$target = urldecode($target)
-$phone = urldecode($phone);
-$date = urldecode($date);
-$number_auto = urldecode($number_auto);
-$extra = urldecode($extra);
+$email = urldecode($email);
 $fio = trim($fio);
-$target = trim($target)
-$phone = trim($phone);
-$date = trim($date);
-$number_auto = trim($number_auto);
-$extra = trim($extra);
+$email = trim($email);
 //echo $fio;
 //echo "<br>";
-//echo $phone;
-//echo $date;
-//echo $number_auto;
-//echo $extra;
-
-if (mail("zubets.andriy@gmail.com", "Заявка с сайта", "ФИО:".$fio.". Тема: ".$target.". Дата: ".$date.". Телефон: ".$phone.". Номер авто: ".$number_auto.". Пожелания к визиту: ".$extra. ,"From: zubets.andriy@gmail.com \r\n"))
+//echo $email;
+if (mail("zubets.andriy@gmail.com", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: zubets.andriy@gmail.com \r\n"))
  {     echo "сообщение успешно отправлено"; 
 } else { 
     echo "при отправке сообщения возникли ошибки";
